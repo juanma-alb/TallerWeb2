cf991b06396f4b133620d17e5ab0d1360e28a8db
lautarodesouches <lautarodesouches@outlook.com>
2025-07-05 11:17:20 -0300

    Estilos productos - traer solo si hay stock

--- frontend/src/app/public/shoe/shoe.component.ts
+++ frontend/src/app/public/shoe/shoe.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Zapatilla } from '../../../interfaces';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-shoe',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './shoe.component.html',
   styleUrl: './shoe.component.css',
 })
