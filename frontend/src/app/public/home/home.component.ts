e9bcec258a6b99cbc1096cce48d878383e434bd8
AgustinAnichini <agustinanichini277@gmail.com>
2025-07-08 12:36:42 -0300

    cambio de slide

--- frontend/src/app/public/home/home.component.ts
+++ frontend/src/app/public/home/home.component.ts
@@ -14,19 +14,18 @@ import { FooterComponent } from '../../shared/footer/footer.component';
 })
 export class HomeComponent {
 
-  sendIcon: string= '/img/iconSend.png'
-  pagoIcon: string= '/img/iconPago.png' 
-  segurityIcon: string= '/img/segurityIcon.png'
-  hombre: string= '/img/hombre.webp'
-  mujer: string= '/img/mujer.webp'
-  ninio: string= '/img/ninio.webp'
-  descatacado1: string= '/img/destacado-1.jpg'
-  descatacado2: string= '/img/destacado-2.jpg'
-  descatacado3: string= '/img/destacado-3.jpg'
+  sendIcon: string = '/img/iconSend.png'
+  pagoIcon: string = '/img/iconPago.png' 
+  segurityIcon: string = '/img/segurityIcon.png'
+  hombre: string = '/img/hombre.webp'
+  mujer: string = '/img/mujer.webp'
+  ninio: string = '/img/ninio.webp'
+  descatacado1: string = '/img/destacado-1.jpg'
+  descatacado2: string = '/img/destacado-2.jpg'
+  descatacado3: string = '/img/destacado-3.jpg'
 
-
-
-   images: any[] = [
+  // Galería 1 (primera)
+  imagesFirstGallery: any[] = [
     {
       itemImageSrc: '/img/hero-img.webp',
       thumbnailImageSrc: 'img/hero-img-thumb.webp',
@@ -45,6 +44,28 @@ export class HomeComponent {
       alt: 'Zapatilla 3',
       title: 'Modelo Urbano 3'
     },
+  ];
+
+  // Galería 2 
+  imagesSecondGallery: any[] = [
+    {
+      itemImageSrc: '/img/destacado-1.webp',
+      thumbnailImageSrc: 'img/hero-img-thumb.webp',
+      alt: 'Adidas superstar',
+      title: 'El Ícono de Estilo y Comodidad'
+    }, 
+    {
+      itemImageSrc: '/img/destacado-3.webp',
+      thumbnailImageSrc: 'img/hero-img-3-thumb.webp',
+      alt: 'Adidas campus',
+      title: 'Clásico Urbana con un Toque Sofisticado'
+    },
+    {
+      itemImageSrc: '/img/destacado-2.webp',
+      thumbnailImageSrc: 'img/hero-img-1-thumb.webp',
+      alt: 'Forum 2000',
+      title: 'Diseño Retro con Toque Moderno'
+    }
   ];
 
   responsiveOptions = [
